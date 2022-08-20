90d13debb44c0f4f5ee9bdf04b7788b65fae97ff
gokulraj <gokulrai02042000@gmail.com>
2022-08-20 14:47:35 +0530

    newly updated

---
+++ mixing.js
@@ -0,0 +1,31 @@
+"use strict";
+//  let water=[1,2,3,4,5];
+// // for(let i=0;i<5;i++)
+// // {
+// //     console.log(water[i]+5);
+// // }
+// for(let i=0;i<water.length;i++)
+// {
+//     if(water[i]%2==0)
+//     {
+//     console.log(water[i]);
+// }}
+// // let water = [1, 2, 3, 4, 5]
+// console.log(
+//     water.map(a => a + 5)
+// );
+// console.log(water.filter(a => a % 2 === 0))
+// function printVal(water:number[]):void{
+//     console.log(water);
+// }
+// printVal(water);
+//array
+let num = [1, 2, 3, 5];
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+// console.log(num[3]);
+//array foreach
+num.forEach(function (value) {
+    console.log(value);
+});
